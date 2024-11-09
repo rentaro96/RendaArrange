7275c1cfb45477fa6366f91b42ad13c97a5726da
Rentaro Suzuki <rentajaxa96@gmail.com>
2024-11-09 16:26:31 +0900

    スペース追加

--- RendaArrange/ViewController.swift
+++ RendaArrange/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.  
     }
     
     @IBAction func tap(){
